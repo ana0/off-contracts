f051f56ecc51d02d93cd316874b35840913ab93c
ana0 <sarah.anne.friend@gmail.com>
2021-04-07 18:10:49 +0200

    First passing test

--- contracts/Off.sol
+++ contracts/Off.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.7.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
@@ -6,6 +7,10 @@ contract Off is ERC721 {
     address public controller;
     address public owner;
     uint256 public price;
+
+    mapping (uint256 => bool) public forSale;
+    mapping (uint256 => string) public imageHash;
+    mapping (uint256 => string) public metadataHash;
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Must be owner");
@@ -24,29 +29,50 @@ contract Off is ERC721 {
 
     constructor (string memory name_, string memory symbol_, uint256 price_) ERC721(name_, symbol_) {
         price = price_;
+        owner = msg.sender;
     }
 
-    function updateController(address _controller) public onlyOwner {
-        controller = _controller;
+    function setController(address controller_) public onlyOwner {
+        controller = controller_;
     }
 
-    function updateOwner(address _owner) public onlyOwner {
-        owner = _owner;
+    function setOwner(address owner_) public onlyOwner {
+        owner = owner_;
+    }
+
+    function setPrice(uint256 price_) public onlyOwner {
+        price = price_;
+    }
+
+    function setBaseURI(string memory baseURI_) public onlyOwner {
+        _setBaseURI(baseURI_);
     }
 
     function withdraw() public onlyOwner {
-
+        bool sent = payable(owner).send(address(this).balance);
+        require(sent, "Failed to send Ether");
     }
 
-    function mint() public onlyOwner {
-
+    function mint(uint256 tokenId, bool forSale_, string memory tokenURI, string memory metadataHash_, string memory imageHash_) public onlyOwner {
+        forSale[tokenId] = forSale_;
+        metadataHash[tokenId] = metadataHash_;
+        imageHash[tokenId] = imageHash_;
+        _safeMint(owner, tokenId);
+        _setTokenURI(tokenId, tokenURI);
     }
 
-    function buy() public {
-
+    function buy(uint256 tokenId, address to) public payable {
+        require(forSale[tokenId]);
+        require(_exists(tokenId), "");
+        require(msg.value >= price);
+        forSale[tokenId] = false;
+        _transfer(owner, to, tokenId);
     }
 
-    function sell() public onlyOwnerOrController {
-
+    function sell(uint256 tokenId, address to) public onlyOwnerOrController {
+        require(forSale[tokenId]);
+        require(_exists(tokenId), "");
+        forSale[tokenId] = false;
+        _transfer(owner, to, tokenId);
     }
 }
--- test/Off.test.js
+++ test/Off.test.js
@@ -8,19 +8,18 @@ require('chai')
   .use(require('chai-bn')(BN))
   .should();
 
-contract('Off', ([_, systemOwner, attacker, owner]) => { // eslint-disable-line no-unused-vars
+contract('Off', ([_, owner, attacker, controller, user]) => { // eslint-disable-line no-unused-vars
   let off = null;
 
   beforeEach(async () => {
-    off = await Off
-      .new(
-        { from: systemOwner },
-      );
+    off = await Off.new("off", "OFF", web3.utils.toWei('35'), { from: owner });
   });
 
-  // it('should successfully birth a lifeform', async () => {
-  //   const tokenId = 0;
-  //   await lifeforms.birth(owner, tokenId, { from: owner });
-  //   (await lifeforms.isAlive(tokenId)).should.be.equal(true);
-  // });
+  it('should successfully mint a token', async () => {
+    const tokenId = 0;
+    const metadataHash = "QmbLnKaAsUbCXx3JYtMTkgXAeAUx2TN8diuy6qFhwN1zE5";
+    const imageHash = "QmRfQakyz9mmKJE8BMQTZMm9QAWsiRp9oroGcyeKFyANid";
+    await off.mint(tokenId, true, "uri", metadataHash, imageHash, { from: owner });
+    (await off.ownerOf(tokenId)).should.be.equal(owner);
+  });
 });
