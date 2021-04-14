3b486fdeb52700b0989603a152871e12a618801d
ana0 <sarah.anne.friend@gmail.com>
2021-04-14 16:45:49 +0200

    Add test config to first migration

--- migrations/2_deploy_main_contract.js
+++ migrations/2_deploy_main_contract.js
@@ -1,5 +1,9 @@
 const Off = artifacts.require('Off');
 
 module.exports = function (deployer) {
-  deployer.deploy(Off, 'Off', 'OFF', web3.utils.toWei('35'));
+  deployer.deploy(Off, 'Off', 'OFF', web3.utils.toWei('35'))
+    .then(async (off) => {
+      await off.setBaseURI('http://localhost:8080/ipfs/');
+      await off.mint(1, true, 'QmYzqt8s6Xmv8HtDdQ69zyfpcQdx89Gi8bnUmEZ4AHrT6z', 'QmYzqt8s6Xmv8HtDdQ69zyfpcQdx89Gi8bnUmEZ4AHrT6z', 'QmaNEWataLJrRPr6WF7yqgaj4yT6VQYufBnM15HVXepwCh');
+    });
 };
